afff87de40cdc10adfb7a45b3960e9a2bf57d230
Behruz <behruz1231.09@gmail.com>
2026-07-23 22:42:58 +0500

    Create config.py

---
+++ tap_earn_bot/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Bot tokeningizni kiriting
+BOT_TOKEN = "8300434192:AAHQ-RvE9I0SsD_i61LEG5a1lZTScGhc8oM"
+
+# Admin Telegram ID-larini kiriting
+ADMIN_IDS = [8488028783]
+
+# Ma'lumotlar bazasi fayli manzili
+DB_PATH = "bot_database.db"
